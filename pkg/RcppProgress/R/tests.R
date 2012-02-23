1b613d2ca065050a9dec875697e158542f6f7635
kforner (Mr ) <karl.forner@gmail.com>
2012-02-23 17:44:24 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwas-bin-tests@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RcppProgress/R/tests.R
@@ -0,0 +1,10 @@
+
+test_sequential <- function(max=100, nb=1000, display_progress=TRUE) {
+	.Call("test_sequential_wrapper", max, nb, display_progress, PACKAGE = "RcppProgress")
+	invisible()
+}
+
+test_multithreaded <- function(max=100, nb=1000, threads=0, display_progress=TRUE) {
+	.Call("test_multithreaded_wrapper", max, nb, threads, display_progress, PACKAGE = "RcppProgress")
+	invisible()
+}
---
+++ pkg/RcppProgress/R/zzz.R
@@ -0,0 +1,14 @@
+#' dynamic loading
+#' @param lib
+#' @param pkg
+#' @import RcppProgress
+#' @author	karl
+
+.onLoad <- function(lib, pkg) {
+	library.dynam("RcppProgress", pkg, lib )
+}
+
+.onUnload <- function (libpath) {
+	cat(".onUnload, libpath=", libpath)
+	library.dynam.unload("RcppProgress", libpath)
+}
