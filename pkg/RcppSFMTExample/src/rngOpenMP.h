99ad9626ee6686abdba7cb321f23aeddb12a1985
kforner (Mr ) <karl.forner@gmail.com>
2012-05-08 17:05:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gwas-bin-tests@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcppSFMTExample/src/rngOpenMP.h
+++
@@ -1,5 +0,0 @@
-void (*createRngArray)(int new_n, int initialize);
-void (*initializeRngInstance)(int k, unsigned int s);
-void (*destroyRngArray)();
-double (*getRngDouble)(int k);
-
--- pkg/RcppSFMTExample/src/rngSFMTOpenMP.h
+++
@@ -1,6 +0,0 @@
-void (*createRngArray)(int newNObj, int initialize);
-void (*initializeRngInstance)(int k, unsigned int s);
-void (*destroyRngArray)();
-double (*getRngDouble)(int k);
-void (*setRngParams)(int initType);
-
